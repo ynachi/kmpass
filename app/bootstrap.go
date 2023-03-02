3f2ab96ad312eecc254e69eef6718dd4c060e80a
Yao Noel Achi <achi.noel@hotmail.com>
2023-03-02 18:54:18 +0000

    chore: update GenerateClusterConfigs method to also return cloud init file path

--- app/bootstrap.go
+++ app/bootstrap.go
@@ -17,40 +17,40 @@ type BootstrapConfig struct {
 
 // updateCloudinitNodes updates the nodes cloudinit file. There is a placeholder in this cloudinit file for a
 // bootstrap shell script which install the prerequisites for kubernetes. This script can be updated with some
-// parameters like Kubernetes version.
-func (config BootstrapConfig) updateCloudinit() error {
+// parameters like Kubernetes version. Returns the cloud init file path in the local machine or an error.
+func (config BootstrapConfig) updateCloudinit() (string, error) {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		Logger.Error("cannot get home dir", err)
-		return ErrGetHomeDirectory
+		return "", ErrGetHomeDirectory
 	}
 	parsedTemplate, err := template.ParseFiles("app/files/clouds.yaml.tpl")
 	if err != nil {
 		Logger.Error("unable to load Go template file file", err, "filename", "files/clouds.yaml.tpl")
-		return ErrLoadTemplate
+		return "", ErrLoadTemplate
 	}
 	err = os.MkdirAll(homeDir+"/kmpass", 0770)
 	if err != nil {
 		Logger.Error("unable to load Go template file file", err, "filename", "files/clouds.yaml.tpl")
-		return ErrLoadTemplate
+		return "", ErrLoadTemplate
 	}
 	outFilePath := filepath.Join(homeDir, "kmpass", "cloudinit.yaml")
 	outFile, err := os.Create(outFilePath)
 	if err != nil {
 		Logger.Error("unable to create file", err, "path", outFilePath)
-		return ErrCreateFile
+		return "", ErrCreateFile
 	}
 	if err := parsedTemplate.Execute(outFile, config); err != nil {
 		Logger.Error("unable to parse template", err)
-		return ErrParseTemplate
+		return "", ErrParseTemplate
 	}
-	return nil
+	return outFilePath, nil
 }
 
 // GenerateClusterConfigs generates configuration files used to bootstrap the cluster. These configuration files
 // include the lb conf, the kubeadm bootstrap yaml file and the nodes cloudinit.
 // returns LBConfPath, kubeadmInitConfPath
-func GenerateClusterConfigs(cluster *Cluster) (string, string, error) {
+func GenerateClusterConfigs(cluster *Cluster) (string, string, string, error) {
 	lbConfPath, err := cluster.generateConfigFromTemplate("app/files/haproxy.cfg.tpl", "haproxy.cfg")
 	if err != nil {
 		Logger.Error("unable to generate lb config file", err, "cluster", cluster.Name)
@@ -63,8 +63,9 @@ func GenerateClusterConfigs(cluster *Cluster) (string, string, error) {
 	encodedBootstrapScript, err := EncodeFileB64("app/files/install.sh")
 	if err != nil {
 		Logger.Error("unable to encode file", err, "filename", "install.sh")
-		return "", "", ErrBase64Encode
+		return "", "", "", ErrBase64Encode
 	}
 	bootstrapConfig.NodeBootstrapScript = encodedBootstrapScript
-	return lbConfPath, kubeadmInitConfPath, bootstrapConfig.updateCloudinit()
+	cloudInitPath, err := bootstrapConfig.updateCloudinit()
+	return lbConfPath, kubeadmInitConfPath, cloudInitPath, err
 }
